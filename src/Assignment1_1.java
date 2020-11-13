418eaba45b68791d780225acf317d46d8978a6f7
Cat Wilber <catherine.wilber@gmail.com>
2020-11-12 18:40:45 -0600

    Test

---
+++ src/Assignment1_1.java
@@ -0,0 +1,16 @@
+class Assignment1_1 {
+    public static void main(String[] args) {
+        String firstName = "Catherine";
+        String lastName = "Wilber";
+        System.out.println(firstName + " " + lastName);
+
+        String course = "Java Web";
+        System.out.println(course);
+
+        String homeTown = "Council Bluffs, IA";
+        System.out.println(homeTown);
+
+        String favoriteFood = "Hot Wings";
+        System.out.println(favoriteFood);
+    }
+}
---
+++ src/Assignment1_2.java
@@ -0,0 +1,15 @@
+class Assignment1_2 {
+    public static void main(String[] args) {
+        String lineOne = "     J     A    V     V    A";
+        System.out.println(lineOne);
+
+        String lineTwo = "     J    A A    V   V    A A";
+        System.out.println(lineTwo);
+
+        String lineThree = "J    J   AAAAA    V V    AAAAA";
+        System.out.println(lineThree);
+
+        String lineFour = " J  J   A     A    V    A     A";
+        System.out.println(lineFour);
+    }    
+}
